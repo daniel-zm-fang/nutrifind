a7809a98e47e5c44a7c06bfdf46fb76a1eb068c0
Daniel Fang <danielfang2001@gmail.com>
2021-09-17 23:07:32 -0400

    init

---
+++ frontend/popup.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let allergicFoodList = [];
+
+  const addButton = document.getElementById('addButton');
+  // const automateButton = document.getElementById('automate_button');
+  // const clearButton = document.getElementById('clear_button');
+  // let lastState = recordButton.innerText;
+  chrome.storage.sync.get('state', (s) => {
+    if (s.allergicFoodList) {
+      allergicFoodList = s.allergicFoodList;
+      recordButton.innerText = lastState;
+    }
+  });
+  // recordButton.addEventListener('click', (e) => {
+  //   if (recordButton.innerText == 'Start Recording') {
+  //     recordButton.innerText = 'Stop Recording';
+  //   } else {
+  //     recordButton.innerText = 'Start Recording';
+  //   }
+  //   chrome.storage.sync.set({ state: recordButton.innerText });
+  //   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  //     chrome.tabs.sendMessage(tabs[0].id, {
+  //       msg: 'record button clicked',
+  //       data: recordButton.innerText,
+  //     });
+  //   });
+  // });
+  // automateButton.addEventListener('click', (e) => {
+  //   if (automateButton.innerText == 'Automate') {
+  //     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  //       chrome.tabs.sendMessage(tabs[0].id, {
+  //         msg: 'automate button clicked',
+  //       });
+  //     });
+  //   } else {
+  //     automateButton.innerText = 'Automate';
+  //   }
+  // });
+  // clearButton.addEventListener('click', (e) => {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  //     chrome.tabs.sendMessage(tabs[0].id, {
+  //       msg: 'clear button clicked',
+  //     });
+  //   });
+  // });
+  // chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  //   console.log(message);
+  // });
+});
