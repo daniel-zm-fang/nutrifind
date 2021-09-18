236bbec8b542369c30ed06e788f121a623d0626c
Daniel Fang <danielfang2001@gmail.com>
2021-09-17 23:58:52 -0400

    add basic add food function

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,49 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
   let allergicFoodList = [];
 
+  const addFoodInputBar = document.getElementById('foodText');
   const addButton = document.getElementById('addButton');
-  // const automateButton = document.getElementById('automate_button');
-  // const clearButton = document.getElementById('clear_button');
-  // let lastState = recordButton.innerText;
-  chrome.storage.sync.get('state', (s) => {
+
+  chrome.storage.sync.get('allergicFoodList', (s) => {
     if (s.allergicFoodList) {
       allergicFoodList = s.allergicFoodList;
-      recordButton.innerText = lastState;
     }
   });
-  // recordButton.addEventListener('click', (e) => {
-  //   if (recordButton.innerText == 'Start Recording') {
-  //     recordButton.innerText = 'Stop Recording';
-  //   } else {
-  //     recordButton.innerText = 'Start Recording';
-  //   }
-  //   chrome.storage.sync.set({ state: recordButton.innerText });
-  //   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  //     chrome.tabs.sendMessage(tabs[0].id, {
-  //       msg: 'record button clicked',
-  //       data: recordButton.innerText,
-  //     });
-  //   });
-  // });
-  // automateButton.addEventListener('click', (e) => {
-  //   if (automateButton.innerText == 'Automate') {
-  //     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  //       chrome.tabs.sendMessage(tabs[0].id, {
-  //         msg: 'automate button clicked',
-  //       });
-  //     });
-  //   } else {
-  //     automateButton.innerText = 'Automate';
-  //   }
-  // });
-  // clearButton.addEventListener('click', (e) => {
-  //   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  //     chrome.tabs.sendMessage(tabs[0].id, {
-  //       msg: 'clear button clicked',
-  //     });
-  //   });
-  // });
-  // chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  //   console.log(message);
-  // });
+
+  addButton.addEventListener('click', (e) => {
+    allergicFoodList.push(addFoodInputBar.value);
+    chrome.storage.sync.set({ allergicFoodList: allergicFoodList }, () => {
+      console.log(allergicFoodList);
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          allergicFoodList: allergicFoodList,
+        });
+      });
+    });
+  });
 });
