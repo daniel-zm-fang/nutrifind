06004edecc90373576f13cbcbab8ff34f6ce8acf
EdwardGa0 <edwardgaobot@gmail.com>
2021-09-18 01:51:24 -0400

    nlp package

--- frontend/nlp.js
+++ frontend/nlp.js
@@ -1,1 +1,9 @@
-function isIngredients() {}
+nlp.extend(compromiseNumbers);
+
+function listIsIngredient(listStr) {
+    let doc = nlp(listStr);
+    let nouns = doc.nouns().out('array');
+}
+let doc = nlp('3 apples');
+console.log(doc.numbers().out('array'));
+console.log('sanity');
