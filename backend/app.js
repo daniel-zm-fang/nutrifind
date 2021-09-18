ed3026e0d483ba4a794433b8b1b43d70c7552063
EdwardGa0 <edwardgaobot@gmail.com>
2021-09-18 16:23:48 -0400

    wip

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,13 @@ const config = require('./utils/config');
 
 const app = express();
 const nutritionRouter = require('./controllers/nutritition');
+const nlpRouter = require('./controllers/nlp');
 
 app.use(express.static('build'));
 app.use(express.json());
 
 app.use('/api/nutrition', nutritionRouter);
+app.use('/api/nlp', nlpRouter);
 
 
 module.exports = app;
