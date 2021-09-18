438d95df1e0954df6e6deaf1c7c25e00029e1a93
sophia <phialee4@gmail.com>
2021-09-18 12:05:55 -0700

    added calorie feature

--- backend/controllers/nutrition.js
+++ backend/controllers/nutrition.js
@@ -1,16 +1,16 @@
 const nutritionRouter = require('express').Router();
+const calories = require('../utils/calories');
 /*
 
 Anything related to routes for the nutrition route
 should go here! Be it sending data back to the caller
 or guiding the caller somewhere.
-
 */
 
 // Example GET route (using localhost:3000/api/nutrition)
 // Async Programming is important here!
 nutritionRouter.get('/', async (request, response) => {
-    response.json({ message: "You have successfuly gett'ed" });
+    response.json({ message: await calories() });
 });
 
 // TODO: Implement route to return JSON with
