97c3e4b9a061de37a0df9a852bdbc0ab7cd371f7
AeGGIndie <37030567+AeGGIndie@users.noreply.github.com>
2021-09-19 00:06:03 -0400

    added route for prices

--- backend/controllers/nutrition.js
+++ backend/controllers/nutrition.js
@@ -1,5 +1,6 @@
 const nutritionRouter = require('express').Router();
 const calories = require('../utils/calories');
+const prices = require('../utils/prices');
 /*
 
 Anything related to routes for the nutrition route
@@ -7,13 +8,15 @@ should go here! Be it sending data back to the caller
 or guiding the caller somewhere.
 */
 
-// Example GET route (using localhost:3000/api/nutrition)
-// Async Programming is important here!
+// GET route for caloric count
 nutritionRouter.get('/', async (request, response) => {
     response.json({ message: await calories() });
 });
 
-// TODO: Implement route to return JSON with
-//       calories, price, stuff like that
+// GET route for price information
+nutritionRouter.get('/prices', async (request, response) => {
+    const { ingredient } = request.body;
+    response.json({ ingredient: await prices(ingredient)});
+});
 
 module.exports = nutritionRouter;
