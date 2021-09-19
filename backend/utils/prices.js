087eb4ebe0bd1c1b7c59c0f77e80f09ba21a3c43
AeGGIndie <37030567+AeGGIndie@users.noreply.github.com>
2021-09-19 01:00:16 -0400

    better'd the scraper

--- backend/utils/prices.js
+++ backend/utils/prices.js
@@ -13,6 +13,11 @@ const args = [
 
 
 const scrapeProduct = (async (ingredient) => {
+    // Error check the ingredient
+    if (!ingredient) {
+        return { message: `The argument provided (${ingredient}) is empty.` }
+    }
+
     // Start puppeteer
     const browser = await puppeteer.launch({
         headless: true,
@@ -22,8 +27,10 @@ const scrapeProduct = (async (ingredient) => {
     // Set user-agent based on system
     const page = await browser.newPage();
 
-    // Create a new page to walmart
+    // Queries to sort walmart products by
     const popularQuery = '&sort=Popular%3ADESC';
+
+    // Create a new page to walmart
     await Promise.all([
         // Go to the ingredient with the popular query
         page.goto(`https://www.walmart.ca/search?q=${ingredient}${popularQuery}`),
@@ -32,8 +39,8 @@ const scrapeProduct = (async (ingredient) => {
     
     // Perform HTML queries to get all the 'products' on the page
     const items = await page.evaluate(() => {
-        const divs = document.querySelectorAll("div[data-automation='product']");
-        return Array.from(divs).map((div => div.children[0].children[0].children[1].innerText));
+        const divs = document.querySelectorAll("div[data-automation='grocery-label']");
+        return Array.from(divs).map((div => div.parentElement.parentElement.parentElement.children[1].innerText));
     });
 
     // Initialize JSON for returning ingredients
@@ -43,12 +50,15 @@ const scrapeProduct = (async (ingredient) => {
     for (const item of items){
         // Parse the product to get the price(s) parts
         const itemInfo = item.split('\n');
+        console.log(itemInfo);
         const priceParsed = itemInfo.filter(item => {
            return (item.includes('¢') || item.includes('$'));
         });
 
         // Name of the product always found at zero'th index
-        const itemName = itemInfo[0];
+        const itemName = itemInfo.filter(item => {
+            return item.toLowerCase().includes(ingredient);
+        })[0];
 
         // Add to the array of products available
         productsInfo.push({
@@ -77,8 +87,9 @@ const scrapeProduct = (async (ingredient) => {
 });
 
 // (async () => {
-//     const p = await scrapeProduct('chicken');
+//     const p = await scrapeProduct('tomato');
 //     console.log(p);
+//     process.exit(1);
 // })();
 
 module.exports = scrapeProduct;
