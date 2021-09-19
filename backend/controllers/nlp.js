3096e6c09e999b232c3da0cc58295c5c74293fc2
EdwardGa0 <edwardgaobot@gmail.com>
2021-09-18 21:14:33 -0400

    Co-authored-by: Daniel Fang <daniel-zm-fang@users.noreply.github.com>

--- backend/controllers/nlp.js
+++ backend/controllers/nlp.js
@@ -1,16 +1,60 @@
 const nlpRouter = require('express').Router();
+const fs = require('fs')
+const nlp = require('compromise');
+nlp.extend(require('compromise-numbers'));
+
+const data = fs.readFileSync('./data/food-related.csv').toLocaleString()
+const allFoodWords = data.split('\r\n')
+const tempWords = allFoodWords.map((word) => {
+    return word.slice(1, -1)
+})
+
+function toFood(nouns) {
+    return nouns.filter((noun) => {
+        return tempWords.indexOf(noun) > -1
+    })
+}
+
 /*
-
-Anything related to routes for the nutrition route
-should go here! Be it sending data back to the caller
-or guiding the caller somewhere.
-
+async function findIngredient(uls) {
+  for (const ul of uls) {
+    const ingredientsList = [];
+    let broken = false;
+    for (const li of ul) {
+      const ingredient = {};
+      const doc = nlp(li);
+      const nums = doc.numbers().json();
+      if (nums.length === 0) {
+        broken = true;
+        break;
+      } else {
+        ingredient.quantity = nums[0].number;
+      }
+      const nouns = doc.nouns().out('array');
+      const foodNouns = await axios.get('localhost:3000/api/nlp', {
+        params: { nouns }
+      });
+      if (foodNouns && foodNouns.length) {
+        ingredient.ingredient = foodNouns.join(' ');
+      } else {
+        broken = true;
+        break;
+      }
+      
+    }
+  }
+}
 */
 
-// Example GET route (using localhost:3000/api/nutrition)
-// Async Programming is important here!
 nlpRouter.get('/isFood', async (request, response) => {
-    response.json({ message: "You have successfuly gett'ed" });
+    response.json({ message: "You have successfuly gett'ed", data: toFood(nouns) });
+});
+
+nlpRouter.post('/isFood', async (request, response) => {
+
+    const nouns = request.body.nouns; // array of string
+    console.log(request.body)
+    response.json({ message: "You have successfuly gett'ed", data: toFood(nouns) });
 });
 
 // TODO: Implement route to return JSON with
--- backend/utils/calories.js
+++ backend/utils/calories.js
@@ -1,85 +1,87 @@
 var axios = require('axios').default;
 var convert  = require('convert-units')
 
-var ingredient = 'chicken';
-var quantity = 200;
-var quantity_type = 'grams';
-quantity_type = quantity_type.toLowerCase();
-
-async function calories() {
+async function quantityHelper(quantity, quantityType) {
+  quantityType = quantityType.toLowerCase();
   try{ 
-    switch (quantity_type){
+    switch (quantityType){
       case 'tbsp':
       case 'tablespoon':
       case 'tablespoons':
-        quantity_type = 'Tbs';
+        quantityType = 'Tbs';
         quantity *= 12;
         break;
       case 'cups':
-        quantity_type = "cup";
+        quantityType = "cup";
         quantity *= 150;
         break;
       case 'tsps':
       case 'teaspoon':
       case 'teaspoons':
-        quantity_type = 'tsp';
+        quantityType = 'tsp';
         quantity *= 4;
         break;
       case 'milliliter':
       case 'millilitre': 
       case 'milliliters': 
       case 'millilitres':
-        quantity_type = 'ml';
+        quantityType = 'ml';
         break;
       case 'liter':
       case 'litre':
       case 'liters':
       case 'litres':
-        quantity_type = 'l';
+        quantityType = 'l';
         quantity *= 900;
         break;
       case 'quart':
       case 'quarts':
       case 'qts':
-        quantity_type = 'qt';
+        quantityType = 'qt';
         quantity *= 1000;
         break;
       case 'gram':
       case 'grams':
-        quantity_type = 'g';
+        quantityType = 'g';
         break;
       case 'milligram': 
       case 'milligrams':
       case 'mgs':
-        quantity_type = 'mg';
+        quantityType = 'mg';
       case 'kilogram':
       case 'kilograms':
       case 'kgs':
-        quantity_type = 'kg';
+        quantityType = 'kg';
         break;
       case 'pounds':
       case 'pound':
       case 'lbs':
-        quantity_type = 'lb';
+        quantityType = 'lb';
         break;
       case 'ounce':
       case 'ounces':
-        quantity_type = 'oz';
+        quantityType = 'oz';
         break;
       default:
         throw new Exception('Invalid units');
-    } 
+    }
+    return { quantity, quantityType }
   } catch (Exception) {
-    //send back to front end
+    return null;
   }
+}
 
-  var unit_possibilities = convert(1).from(quantity_type).possibilities();
+async function calories(ingredient, quantity, quantityType) {
+  let temp = { quantity, quantityType };
+  temp = quantityHelper()
+
+  var unit_possibilities = convert(1).from(quantityType).possibilities();
   if(unit_possibilities.includes('g')){
-    convert(1).from(quantity_type).to('g');
+    convert(1).from(quantityType).to('g');
   }
   quantity /= 100;
 
-
+asdfasdfasdfasdffff
 
 
   var options = {
