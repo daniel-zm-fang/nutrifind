fb38764829e95945b729676e93097f400c79b33d
sophia <phialee4@gmail.com>
2021-09-19 02:44:23 -0700

    edited navigation tab

--- backend/utils/calories.js
+++ backend/utils/calories.js
@@ -12,16 +12,19 @@ async function calories() {
       case 'tbsp':
       case 'tablespoon':
       case 'tablespoons':
+      case 'Tbs':
         quantity_type = 'Tbs';
         quantity *= 12;
         break;
       case 'cups':
+      case 'cup':
         quantity_type = "cup";
         quantity *= 150;
         break;
       case 'tsps':
       case 'teaspoon':
       case 'teaspoons':
+      case 'tsp':
         quantity_type = 'tsp';
         quantity *= 4;
         break;
@@ -29,47 +32,54 @@ async function calories() {
       case 'millilitre': 
       case 'milliliters': 
       case 'millilitres':
+      case 'ml':
         quantity_type = 'ml';
         break;
       case 'liter':
       case 'litre':
       case 'liters':
       case 'litres':
+      case 'l':
         quantity_type = 'l';
         quantity *= 900;
         break;
       case 'quart':
       case 'quarts':
       case 'qts':
+      case 'qt':
         quantity_type = 'qt';
         quantity *= 1000;
         break;
       case 'gram':
       case 'grams':
+      case 'g':
         quantity_type = 'g';
         break;
       case 'milligram': 
       case 'milligrams':
       case 'mgs':
+      case 'mg':
         quantity_type = 'mg';
       case 'kilogram':
       case 'kilograms':
       case 'kgs':
+      case 'kg':
         quantity_type = 'kg';
         break;
       case 'pounds':
       case 'pound':
       case 'lbs':
+      case 'lb':
         quantity_type = 'lb';
         break;
       case 'ounce':
       case 'ounces':
+      case 'oz':
         quantity_type = 'oz';
         break;
       default:
         throw new Exception('Invalid units');
     } 
-    //returns 0 calories if not a valid unit
   } catch (Exception) {
     quantity = 0;
   }
